d1500c91d25e6c291f9a1d30a6cb0e9e87046105
danielfrance <dan@ittybam.com>
2014-03-06 15:42:05 -0800

    first ninja_gold commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php 
+session_start();
+ ?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ninja Gold Game</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<!-- Player Score -->
+			<nav id="header" class="navbar navbar-default">
+				<div id="counter" class="navbar-header" >
+					<h1>Your Gold</h1>
+					<div style="border: 1px solid grey" class=" form-control">
+					<?php 
+						if (isset($_SESSION['total_gold'])) 
+						{
+							echo $_SESSION['total_gold'];
+						}
+					 ?>
+					</div>
+				</div>
+				<div value="total_gold" class="navbar">
+					 
+
+				</div>
+			</nav>
+			<!-- Game Play -->
+			<div id="main-content" class="row">
+				<h3>Click to Play!</h3>
+					<div class="row col-md-offset-1">
+						<div class="building col-sm-2 col-md-2">
+								<form action="process.php" method="post">
+								<h3 class="text-center">Farm</h3>
+								<h5 class="text-center">(Earns 10-20 Gold)</h5>
+								<input type="hidden" name="action" value="farm"> 
+								<input class="center-block" type="submit">
+							</form>
+						</div> 
+
+						 <div class="building col-sm-2 col-md-2 col-md-offset-1">
+							<form action="process.php" method="post">
+								<h3 class="text-center">Cave</h3>
+								<h5 class="text-center">(Earns 5-10 Gold)</h5>
+								<input type="hidden" name="action" value="cave">
+								<input class="center-block" type="submit">
+							</form>
+						</div> 
+
+						<div class="building col-sm-2 col-md-2 col-md-offset-1">
+							<form action="process.php" method="post">
+								<h3 class="text-center">House</h3>
+								<h5 class="text-center">(Earns 2-5 Gold)</h5>
+								<input type="hidden" name="action" value="house">
+								<input class="center-block" type="submit">
+							</form>
+						</div>
+
+						<div class="building col-sm-2 col-md-2 col-md-offset-1 ">
+							<form action="process.php" method="post">
+								<h3 class="text-center">Casino</h3>
+								<h5 class="text-center">(Earns 0-50 Gold)</h5>
+								<input type="hidden" name="action" value="casino">
+								<input class="center-block" type="submit">
+							</form>
+						</div>
+					</div>
+			</div>
+			<!-- Activity Log -->
+ 			<div id="activities_container" class="container">
+				<h5>Activities:</h5>
+				<div id="activities_log" class="row">
+						<?php 
+						if (isset($_SESSION['activities'])) 
+						{
+							foreach ($_SESSION['activities'] as $number => $message) 
+							{
+								echo '<p>' .  $message;
+							}
+						}
+						else
+						{
+							echo "<p> you havent' played the game yet. get on it.";
+						}
+						 ?>
+				</div>
+			</div> 
+			<div>
+				<form action="process.php" method="post" class="navbar-form navbar-right">
+					 	<div class="form-group">
+					 		<input class="form-control" type="hidden" name="action" value="reset">
+					 		<input class="btn btn-warning" type="submit" value="RESET">
+					 	</div>
+					 	
+					 </form>
+			</div>
+	</div>
+</body>
+</html>
+<?php 
+
+ ?>
---
+++ process.php
@@ -0,0 +1,76 @@
+<?php 
+	session_start();
+	// server timezone
+	
+	date_default_timezone_set('America/Los_Angeles');
+	$time_stamp = date("F j Y h:i");
+
+	if (isset($_POST['action']) && $_POST['action'] == 'reset')  
+	{
+		session_destroy();
+		header('location: index.php');
+	}
+
+	if (!isset($_SESSION['activities']))
+	{
+		$_SESSION['activities'] = array();
+		$_SESSION['total_gold'] = 0;
+		
+	}		
+
+	if (isset($_POST['action']) && $_POST['action'] == 'farm') 
+			{
+				$random_gold = rand(10,20); // random gold is generated
+				
+						$_SESSION['total_gold'] = $_SESSION['total_gold'] + $random_gold;
+						array_unshift($_SESSION['activities'], '<p class="won">You entered a ' .  $_POST['action'] . " and you earned " . $random_gold . " gold" . "(" .  $time_stamp . ")</p>");
+						header('location: index.php');
+						
+
+				}
+	if (isset($_POST['action']) && $_POST['action'] == 'cave') 
+			{
+				$random_gold = rand(5,10); // random gold is generated
+				
+						$_SESSION['total_gold'] = $_SESSION['total_gold'] + $random_gold;
+						array_unshift($_SESSION['activities'], '<p class="won">You entered a ' .  $_POST['action'] . " and you earned " . $random_gold . " gold" . "(" .  $time_stamp . ")</p>");
+						header('location: index.php');
+					
+
+				}
+
+	if (isset($_POST['action']) && $_POST['action'] == 'house') 
+	{
+		$random_gold = rand(2,5); // random gold is generated
+		
+				$_SESSION['total_gold'] = $_SESSION['total_gold'] + $random_gold;
+				array_unshift($_SESSION['activities'], '<p class="won">You entered a ' .  $_POST['action'] . " and you earned " . $random_gold . " gold" . "(" .  $time_stamp . ")</p>");
+				header('location: index.php');
+			
+
+		}
+
+if (isset($_POST['action']) && $_POST['action'] == 'casino') 
+	{
+		$random_gold = rand(0,50); // random gold is generated
+		$random_chance = rand(1,2); // random chance is generated
+		if ($random_chance > 1) // the chance determines if the user wins gold or loses it
+			{
+				$_SESSION['total_gold'] = $_SESSION['total_gold'] -  $random_gold;
+			array_unshift($_SESSION['activities'], '<p class="lost">You entered a ' .  $_POST['action'] . " and you lost " . $random_gold . " gold" . "(" .  $time_stamp . ")</p>");
+				header('location: index.php');
+			}
+		else
+			{
+				$_SESSION['total_gold'] = $_SESSION['total_gold'] + $random_gold;
+				array_unshift($_SESSION['activities'], '<p class="won">You entered a ' .  $_POST['action'] . " and you earned " . $random_gold . " gold" . "(" .  $time_stamp . ")</p>");
+				header('location: index.php');
+			}
+
+		}
+
+		
+
+
+
+ ?>
